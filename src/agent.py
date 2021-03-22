9ca93653c83128b4b68683772c0579daa891490a
bhavesha <bhavesha@dosepack.com>
2021-03-22 11:26:04 +0530

    :memo: some methods in agent class made compulsory to override

--- src/agent.py
+++ src/agent.py
@@ -1,4 +1,6 @@
+import time
 from src.planner import Planner
+from logging_module.logging_module import setup_logger
 
 
 class Agent:
@@ -10,8 +12,9 @@ class Agent:
 
         self.planner = Planner.get_instance()
 
-        self._current_position = current_position
+        self._current_position = self.planner.get_node(node=current_position)
         self._destination = None
+        self._reached = False
 
         self.actions_to_be_taken = list()
 
@@ -31,15 +34,59 @@ class Agent:
     def destination(self, position):
         self._destination = position
 
+    @property
+    def reached(self):
+        return self._reached
+
+    @reached.setter
+    def reached(self, data):
+        self._reached = data
+
     def go_to_position(self, destination=None):
-        self.destination = destination
+        raise NotImplementedError(f'Override this method in {self.__class__.__name__} class')
 
     def provide_actions(self, actions: list):
-        self.actions_to_be_taken.extend(actions)
+        raise NotImplementedError(f'Override this method in {self.__class__.__name__} class')
+
+    def make_movement(self, move):
+        raise NotImplementedError(f'Override this method in {self.__class__.__name__} class')
 
 
 class Car(Agent):
 
     def __init__(self, agent_id=None, name='', current_position=None):
+
         Agent.__init__(self, agent_id=agent_id, name=name, current_position=current_position)
-        pass
+
+        logger_name = f'{self.__class__.__name__}-{self.id}'
+        self.logger = setup_logger(logger_name)
+
+    @property
+    def current_position(self):
+        return self._current_position
+
+    @current_position.setter
+    def current_position(self, position):
+        if type(position) == str:
+            position = self.planner.get_node(node=position)
+        self.logger.info(f'current_position: {self._current_position}')
+        self._current_position = position
+
+    @property
+    def destination(self):
+        return self._destination
+
+    @destination.setter
+    def destination(self, position):
+        if type(position) == str:
+            position = self.planner.get_node(node=position)
+        self._destination = position
+        self.logger.info(f'destination: {self._destination}')
+
+    @property
+    def reached(self):
+        return self._reached
+
+    @reached.setter
+    def reached(self, data):
+        self._reached = data
