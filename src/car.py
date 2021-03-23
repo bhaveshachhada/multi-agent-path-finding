cc9cad3ff3c630df092e979f1d08bdf8f8da4a9c
bhavesha <bhavesha@dosepack.com>
2021-03-23 12:45:23 +0530

    :memo: car.py is now a FSM

--- src/car.py
+++ src/car.py
@@ -1,15 +1,36 @@
+import time
+from fsm.fsm import FSM
+from threading import Event
 from src.agent import Agent
+from enum import Enum, unique
 from logging_module.logging_module import setup_logger
 
 
-class Car(Agent):
+@unique
+class CarState(Enum):
+    IDLE = 1
+    FIND_DIRECTION = 2
+    MOVE = 3
+
+
+class Car(Agent, FSM):
 
     def __init__(self, agent_id=None, name='', current_position=None):
 
         Agent.__init__(self, agent_id=agent_id, name=name, current_position=current_position)
+        FSM.__init__(name=f'agent:{agent_id}&{name}')
 
         logger_name = f'{self.__class__.__name__}-{self.id}'
         self.logger = setup_logger(logger_name)
+
+        self.add_fsm_states()
+        self.current_state = CarState.IDLE
+
+    def add_fsm_states(self):
+        self.add_state(state=CarState.IDLE, default_next_state=CarState.IDLE, routine=self.handler_idle)
+        self.add_state(state=CarState.MOVE, default_next_state=CarState.MOVE, routine=self.handler_move)
+        self.add_state(state=CarState.FIND_DIRECTION, default_next_state=CarState.FIND_DIRECTION,
+                       routine=self.handler_find_direction)
 
     @property
     def current_position(self):
@@ -48,4 +69,16 @@ class Car(Agent):
         pass
 
     def provide_actions(self, actions: list):
+        pass
+
+    def handler_idle(self):
+        self.logger.info(f'In state: {self.current_state.name}')
+        pass
+
+    def handler_find_direction(self):
+        self.logger.info(f'In state: {self.current_state.name}')
+        pass
+
+    def handler_move(self):
+        self.logger.info(f'In state: {self.current_state.name}')
         pass
