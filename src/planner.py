49b17e7cabb4efe18bd8373a074e4daa3f219684
bhavesh <bhaveshachhada.ab@gmail.com>
2023-10-05 17:44:53 +0530

    :x: removed Layout class from planner.py

--- src/planner.py
+++ src/planner.py
@@ -10,68 +10,6 @@ from src.utility import load_json
 MIN_COST = -10000
 
 
-class Layout(Singleton):
-
-    def __init__(self):
-
-        Singleton.__init__(self)
-
-        self.graph_file = "data/graph.json"
-        self.graph = self.load_graph()
-
-        self.logger = setup_logger('graph')
-
-        # note: used to block a node by an agent
-        self.block_node_map = LockedDefaultDict(lambda: None)
-
-        # note: node can be used for navigation by other agents but can't be set as destination by other agents
-        self.semi_block_node_map = LockedDefaultDict(lambda: None)
-
-        # note: added for future use, currently experiment without semi block
-        self.semi_block_node_queue = LockedDefaultDict(list)
-
-    def load_graph(self):
-        return load_json(self.graph_file)
-
-    def block_node(self, node, agent_id):
-        if self.block_node_map[node] is None:
-            self.block_node_map[node] = agent_id
-            self.logger.info(f'{node} blocked by {agent_id}')
-        else:
-            self.logger.info(f'{node} cannot be blocked by {agent_id}, {self.block_node_map[node]} has blocked')
-
-    def unblock_node(self, node):
-        # self.block_node_map[node] = None
-        agent_id = self.block_node_map.pop(node)
-        self.logger.info(f'{node} unblocked by {agent_id}')
-
-    def is_node_free(self, node):
-        return self.block_node_map[node] is None
-
-    def is_node_blocked_for_agent(self, node, agent_id):
-        return self.block_node_map[node] != agent_id
-
-    def is_node_blocked_by_agent(self, node, agent_id):
-        return self.block_node_map[node] == agent_id
-
-    def semi_block_enable(self, node):
-        agent_id = self.semi_block_node_queue[node][0]
-        self.semi_block_node_map[node] = agent_id
-
-    def semi_block_disable(self, node):
-        self.semi_block_node_queue[node].pop(0)
-        self.semi_block_node_map.pop(node)
-
-    def request_semi_block(self, node, agent_id):
-        if agent_id not in self.semi_block_node_queue[node]:
-            self.semi_block_node_queue[node].append(agent_id)
-            if len(self.semi_block_node_queue[node]) == 1:
-                self.semi_block_enable(node=node)
-
-    def withdraw_semi_block(self, node):
-        self.semi_block_disable(node=node)
-
-
 class Planner(Singleton):
 
     def __init__(self, path_finder=None):
