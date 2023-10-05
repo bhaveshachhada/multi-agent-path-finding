5ceaab489a640db161ca7b1a9ef800698211e7c7
bhavesh <bhaveshachhada.ab@gmail.com>
2023-10-05 17:45:58 +0530

    :memo: removed dependency from planner

--- algorithms/path_finder.py
+++ algorithms/path_finder.py
@@ -6,8 +6,5 @@ from src.path import Path
 
 class PathFinder:
 
-    def __init__(self, planner=None):
-        self.planner = planner
-
     def find_path(self, source: Node, destination: Node) -> Tuple[bool, Path]:
         raise Exception("Not implemented")
