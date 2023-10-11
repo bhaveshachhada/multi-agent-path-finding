e2f461adc0dc2185090a26bff38bfa509b2308ef
bhavesh <bhaveshachhada.ab@gmail.com>
2023-10-11 22:59:40 +0530

    :heavy_plus_sign: created thread_safe decorator inside Node class

--- src/node.py
+++ src/node.py
@@ -1,5 +1,8 @@
+import threading
+import traceback
 from collections import defaultdict
-from typing import List
+from functools import wraps
+from typing import List, Callable
 
 
 class Node:
@@ -13,6 +16,26 @@ class Node:
         self._blocked = False
         self.connected_nodes_map = defaultdict(lambda: None)
         self.connected_nodes: List['Node'] = list()
+        self.lock = threading.RLock()
+
+    @staticmethod
+    def thread_safe(function: Callable):
+
+        @wraps(function)
+        def inner(self, *args, **kwargs):
+
+            output = None
+            try:
+                self.lock.acquire()
+                output = function(self, *args, **kwargs)
+            except Exception as e:
+                print(f'Error while executing {function.__name__}: {e}')
+                print(traceback.format_exc())
+            finally:
+                self.lock.release()
+                return output
+
+        return inner
 
     def __str__(self):
         return self.name
@@ -36,17 +59,21 @@ class Node:
     def blocked(self, data):
         self._blocked = data
 
+    @thread_safe
     def add_neighbour(self, node, direction):
         self.connected_nodes_map[direction] = node
         self.connected_nodes.append(node)
 
+    @thread_safe
     def get_neighbour(self, direction):
         return self.connected_nodes_map[direction]
 
+    @thread_safe
     def block(self, agent_id):
         self.agent = agent_id
         self.blocked = True
 
+    @thread_safe
     def unblock(self):
         agent_id = self.agent
         self.agent = None
